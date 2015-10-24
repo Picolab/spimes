df4f1d1255d2816d175545d579c3cb08f0f878df
Adam Burdett <burdettadam@gmail.com>
2015-10-23 23:29:39 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,9 +18,23 @@ app.config(function($routeProvider){
 
 });
 
-
 app.controller('cfgController',function($scope){
 
       $scope.message="Hello world";
 
 });
+
+var myApp = angular.module('myApp', ['infinite-scroll']);
+myApp.controller('DemoController', function($scope) {
+  $scope.images = [1, 2, 3, 4, 5, 6, 7, 8];
+
+  $scope.loadMore = function() {
+    var last = $scope.images[$scope.images.length - 1];
+    for(var i = 1; i <= 8; i++) {
+      $scope.images.push(last + i);
+    }
+  };
+});
+
+
+
