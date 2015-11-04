871528380746e73186ed006207af60ab64ee2890
Adam Burdett <burdettadam@gmail.com>
2015-11-04 16:15:01 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,4 @@
-var app=angular.module('single-page-app',['ngRoute']);
 
-
-app.config(function($routeProvider){
-
-
-      $routeProvider
-          .when('/',{
-                templateUrl: 'pages/home.html'
-          })
-          .when('/spimes',{
-                templateUrl: 'pages/spimes.html'
-          })
-          .when('/about',{
-                templateUrl: 'pages/about.html'
-          });
-
-
-});
 
 
 
