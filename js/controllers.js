6ac3981f636cf6e1de66526fe635c1893a479f92
Adam Burdett <burdettadam@gmail.com>
2015-11-04 16:15:20 -0700

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,25 @@
 'use strict';
 
 /* Controllers */
+var app=angular.module('single-page-app',['ngRoute']);
+
+
+app.config(function($routeProvider){
+
+
+      $routeProvider
+          .when('/',{
+                templateUrl: 'pages/home.html'
+          })
+          .when('/spimes',{
+                templateUrl: 'pages/spimes.html'
+          })
+          .when('/about',{
+                templateUrl: 'pages/about.html'
+          });
+
+
+});
 
 app.controller('cfgController',function($scope){
 
